6e5981b656d856115efda74ec626b6aa9b1ae24b
andre-violin <andre.violin@ifms.edu.br>
2024-05-23 13:26:29 -0400

    chore: config prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,30 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    browser: true,
+    es2021: true
   },
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'standard',
+    'prettier'
   ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['vue'],
+  rules: {}
 }
